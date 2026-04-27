60b352cf793760230d5c8722672e8707a1fcdb35
mm23jg-uol <mm23jg@leeds.ac.uk>
2026-04-27 21:37:02 +0100

    Add isValidTridiagonal function to Tridiagonals.java

--- problemcode_fgp/Tridiagonals.java
+++ problemcode_fgp/Tridiagonals.java
@@ -7,4 +7,22 @@ static double[][] exampleMatrix(int n)
         double m[][] = new double [1][1]; //dummy code: write your own
         return m; //dummy code: write your own
     }
+
+public static boolean isValidTridiagonal (double[][] a) {
+    if (a == null) {
+        return false;
+    }
+    if (a.length != 3) {
+        return false;
+    }
+    if (a[0] == null || a[1] == null || a[2] == null) {
+    return false;
+    }
+    if (a[1].length < 1 || a[2].length != a[1].length || a[0].length != a[1].length) {
+        return false;
+    }
+    else {
+        return true;
+    }
+    }
 }
