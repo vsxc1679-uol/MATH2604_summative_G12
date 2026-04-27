1a2e19250947c5328a8a020fa3f3058b6a827770
Shasha Brown <djph7758@leeds.ac.uk>
2026-04-27 19:40:34 +0100

    Adding sum function for Diagonals, as well as incorporating functions to reject null parameters and matrix sizes which are incompatible

--- problemcode_fgp/Diagonals.java
+++ problemcode_fgp/Diagonals.java
@@ -2,5 +2,19 @@ import static java.lang.Math.*;
 
 class Diagonals
 {
-
+    static double[] sum(double[] a, double[] b)
+    {
+        if (a==null || b==null)
+        {return null;}
+        
+        if (a.length != b.length) 
+        {return null;}
+        
+        double [] result = new double[a.length];
+        
+        for (int i=0; i<a.length; i++) 
+        {result[i] = a[i] + b[i];}
+        
+        return result;
+    }
 }
