c845f7ea47368bd4b98e498efedc6b7d0b45c687
Shasha Brown <djph7758@leeds.ac.uk>
2026-04-27 21:26:05 +0100

    Adding product with diagonal, where D is a diagonal matrix and T is the tridiagonal matrix. If an input is missing or the diagonal and the tridiagonal matrix are incompatible, the function returns null. Otherwise the tridiagonal rows (main, upper, lower diagonals) are multiplied by the diagonals matrix  value corresponding to its row. Finally it is stored and return the same was as a tridiagonal matrix.

--- problemcode_fgp/Tridiagonals.java
+++ problemcode_fgp/Tridiagonals.java
@@ -15,9 +15,41 @@ static double[][] exampleMatrix(int n)
         for (int i=0;i<n-1; i++)
         {result[0][i] = 1;
         result[2][i] = i+2;}
+        
         return result;
         }
         //double m[][] = new double [1][1]; //dummy code: write your own
         //return m; //dummy code: write your own
+static double[][] productWithDiagonal(double[] d, double[][] t)
+    {
+        if (d==null || t==null) 
+        {return null;}
+
+        if (t.length != 3) 
+        {return null;}
+
+        if (t[0] == null || t[1] == null || t[2] == null) 
+        {return null;}
+
+        int n = d.length; 
+        
+        if (n==0) 
+        {return null;}
+        
+        if (t[0].length != n || t[1].length != n || t[2].length !=n)
+        {return null;}
+        
+        double[][] result = new double[3][n];
+        
+        for (int i=0; i<n; i++)
+        {result[1][i] = d[i]*t[1][i];}
+        
+        for (int i=0; i<n-1; i++)
+        {result[0][i] = d[i]*t[0][i];
+         result[2][i] = d[i+1]*t[2][i];}
+         
+        return result;
+    } 
     }
+    
 
