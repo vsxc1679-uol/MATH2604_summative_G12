34cc26ce97857de4c3eb9571aed89352b3bc39be
Shasha Brown <djph7758@leeds.ac.uk>
2026-04-27 20:38:55 +0100

    If the tridiagonal matrix of size n is given a value equal or less than 0, null is returned. Otherwise it builds the example matrix from the breif. It stores the matrix in three rows, where result[1][i] is the main diagonal, result[0][i] is the upper diagonal (last entry in ignored), and result[2][i] is the lower diagonal (last entry also ignored)

--- problemcode_fgp/Tridiagonals.java
+++ problemcode_fgp/Tridiagonals.java
@@ -4,7 +4,20 @@ class Tridiagonals
 {
 static double[][] exampleMatrix(int n) 
     {
-        double m[][] = new double [1][1]; //dummy code: write your own
-        return m; //dummy code: write your own
+        if (n<=0) 
+        {return null;}
+        
+        double [][] result = new double[3][n];
+        
+        for (int i=0;i<n; i++)
+        {result[1][i] = -(i+1)*(i+1);}
+        
+        for (int i=0;i<n-1; i++)
+        {result[0][i] = 1;
+        result[2][i] = i+2;}
+        return result;
+        }
+        //double m[][] = new double [1][1]; //dummy code: write your own
+        //return m; //dummy code: write your own
     }
-}
+
