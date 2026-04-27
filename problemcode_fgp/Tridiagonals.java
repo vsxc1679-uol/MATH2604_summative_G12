2b413b0bad01b43ddc75431e2d5e480dadb7c654
Shasha Brown <djph7758@leeds.ac.uk>
2026-04-27 21:52:28 +0100

    Adding java comments for Tridiagonals.java

--- problemcode_fgp/Tridiagonals.java
+++ problemcode_fgp/Tridiagonals.java
@@ -2,52 +2,61 @@ import static java.lang.Math.*;
 
 class Tridiagonals
 {
+        //double m[][] = new double [1][1]; //dummy code: write your own
+        //return m; //dummy code: write your own
 static double[][] exampleMatrix(int n) 
     {
         if (n<=0) 
         {return null;}
-        
+        // if n=0 or less it is not a valid matrix, hence null is returned
         double [][] result = new double[3][n];
-        
+        //Creating storage for the new matrix.
+        //[3][n] accounts for the main, upper and lower diagonal, where each has
+        //length n
         for (int i=0;i<n; i++)
+        //loops from 0 to n-1
         {result[1][i] = -(i+1)*(i+1);}
-        
+        //fills main diagonal (e.g. i=0 -> -(1*1) = -1
         for (int i=0;i<n-1; i++)
+        //loops from 0 to n-2 since upper and lower diagonals are shorter by 1
         {result[0][i] = 1;
+        //fills upper diagonals with 1
         result[2][i] = i+2;}
-        
+        //fills lower diagonal (e.g. i=0 -> 2)
         return result;
         }
-        //double m[][] = new double [1][1]; //dummy code: write your own
-        //return m; //dummy code: write your own
+
 static double[][] productWithDiagonal(double[] d, double[][] t)
+// d -> diagonal matrix
+// t -> tridiagonal matrix
     {
         if (d==null || t==null) 
         {return null;}
-
+        // if inputs dont exists, return null
         if (t.length != 3) 
         {return null;}
-
+        // a valid tridiagonal matrix should have 3 rows
         if (t[0] == null || t[1] == null || t[2] == null) 
         {return null;}
-
+        //checks to see whether the main, upper or lower values are missing
         int n = d.length; 
-        
+        //assigning the size of the matrix to a variable
         if (n==0) 
         {return null;}
-        
+        // if n=0,  it is not a valid matrix, hence null is returned
         if (t[0].length != n || t[1].length != n || t[2].length !=n)
         {return null;}
-        
+        // ensures that the matrix and tridiagonal matrix are the same size
         double[][] result = new double[3][n];
-        
+        //creates an empty array for the result
         for (int i=0; i<n; i++)
         {result[1][i] = d[i]*t[1][i];}
-        
+        //multiplying each tridiagonal main entry with the diagonals
         for (int i=0; i<n-1; i++)
         {result[0][i] = d[i]*t[0][i];
          result[2][i] = d[i+1]*t[2][i];}
-         
+        //multiplying the upper with d[i] and lower diagonals with d[i+1]
+        //diagonal matrix essentially acts as a row scaler
         return result;
     } 
     }
