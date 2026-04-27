640a8e338c4567eef6759f34162cfe181768963a
Shasha Brown <djph7758@leeds.ac.uk>
2026-04-27 20:11:48 +0100

    identical as sum, except it multiplies instead of adding. If either input is missing or matrix sizes dont match, it returns null,creates a new array, then loops through each entry and multiplies

--- problemcode_fgp/Diagonals.java
+++ problemcode_fgp/Diagonals.java
@@ -17,4 +17,19 @@ class Diagonals
         
         return result;
     }
+    static double[] product(double[] a, double[]b)
+    {
+        if (a==null || b==null)
+        {return null;}
+        
+        if (a.length != b.length) 
+        {return null;}
+        
+        double[]result = new double[a.length];
+        
+        for (int i=0; i<a.length; i++)
+        {result[i] = a[i]*b[i];}
+        
+        return result;
+    }
 }
